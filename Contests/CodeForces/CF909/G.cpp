9558955734cb91f29dadfb94844897825bc3cd52
Alvin Hsu <a1hsu@ucsd.edu>
2023-11-18 14:43:34 -0800

    CF909 G

--- Contests/CodeForces/CF909/G.cpp
+++ Contests/CodeForces/CF909/G.cpp
@@ -16,6 +16,53 @@ using namespace std;
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
+	int t;
+	cin >> t;
+	while(t--){
+		int n,q;
+		cin >> n >> q;
+		vector<vector<int>> g(n+1);
+		vector<set<int>> s(n+1);
+		for (int i = 0; i < n - 1; i++){
+			int u,v;
+			cin >> u >> v;
+			g[u].push_back(v);
+			g[v].push_back(u);
+		}
+		vector<int> idx(n+1);
+		vector<vector<array<int,3>>> at(n+1);
+		for (int i = 0; i < n; i++){
+			int p;
+			cin >> p;
+			idx[p] = i+1;
+		}
+		for (int i = 0; i < q; i++){
+			int l,r,x;
+			cin >> l >> r >> x;
+			at[x].push_back({l,r,i});
+		}
+		auto merge = [&](set<int> &s1, set<int>&s2){
+			if (s1.size() < s2.size()) swap(s1, s2);
+			for (auto i : s2) s1.insert(i);
+		};
+		vector<int> ans(q);
+		function<void(int,int)> dfs = [&](int u, int p){
+			s[u].insert(idx[u]);
+			for (auto &v : g[u]){
+				if (v == p) continue;
+				dfs(v, u);
+				merge(s[u], s[v]);
+			}
+			for (auto [l,r,i] : at[u]){
+				auto it = s[u].lower_bound(l);
+				ans[i] = it != s[u].end() && *it <= r; 
+			}
+		};
+		dfs(1,1);
+		for (int i = 0; i < q; i++){
+			cout << (ans[i]?"YES":"NO") << '\n';
+		}
+		cout << '\n';
+	}
 	return 0;
 }
